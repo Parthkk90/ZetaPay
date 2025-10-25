adb1bab823cc671244c6c60621a2236a02d2b2dd
parthkk <parth122004@gmail.com>
2025-10-25 22:31:10 +0530

    Configure WalletConnect

--- web/src/wagmi.ts
+++ web/src/wagmi.ts
@@ -3,6 +3,6 @@ import { bscTestnet, sepolia, zetachainAthensTestnet } from "wagmi/chains";
 
 export const config = getDefaultConfig({
   appName: "RainbowKit demo",
-  projectId: "YOUR_PROJECT_ID",
+  projectId: "9dd3b957e87a350c83ab1b87a7fcf40c",
   chains: [sepolia, bscTestnet, zetachainAthensTestnet],
 });
